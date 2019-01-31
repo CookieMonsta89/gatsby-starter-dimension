847de2f8638bb120f5c895abeb819231a20e9c92
CookieMonsta89 <jlstan06@hotmail.com>
2019-01-31 03:33:39 -0500

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const headerSpan = {
+    fontSize: '40px',
+    fontWeight: 'bold'
+
+}
+
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span className="icon fa-diamond"></span>
+            <span style={headerSpan}>J S</span>
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Joseph Stanfield</h1>
+                <p>Full-Stack Web Developer</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Projects</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Resume</a></li>
             </ul>
         </nav>
     </header>
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,51 @@ import PropTypes from 'prop-types'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+import react from '../images/react.png'
+import boot from '../images/boot.png'
+import js from '../images/js.png'
+import less from '../images/less.png'
+import node from '../images/node.png'
+import sql from '../images/sql.jpg'
+import { auto } from 'eol';
+
+
+const Title = {
+  margin: '0 auto',
+  marginBottom: '30px'
+
+}
+
+const UL = {
+  display: 'flex',
+  justifyContent: 'space-evenly',
+  marginBottom:'30px'
+}
+
+const About = {
+  fontFamily: 'source sans pro',
+  fontSize: '1.2rem',
+  marginBottom: '10px'
+}
+const AboutP = {
+  fontFamily: 'source sans pro',
+  fontSize: '1rem',
+  marginBottom: '20px'
+}
+
+const imageWork = {
+  height:'40px',
+  width:'9%'
+}
+
+const headerThree = {
+  textDecoration: 'underline'
+}
+
+const Resume = {
+  height: '100%'
+}
+
 
 class Main extends React.Component {
   render() {
@@ -12,60 +57,77 @@ class Main extends React.Component {
 
     return (
       <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
-
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
-          {close}
-        </article>
-
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Work</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          <h2 style={Title} className="major">Projects</h2>
+          <div style={UL} className="icons">
+            <img style={imageWork} src={react} alt="" />
+            <img style={imageWork} src={boot} alt="" />
+            <img style={imageWork} src={js} alt="" />
+            <img style={imageWork} src={less} alt="" />
+            <img style={imageWork} src={node} alt="" />
+          </div>
+          <div>
+            <a href='https://fitmetrix.netlify.com/'>
+              <h3 style={headerThree}>Fitmetrix</h3>
+            </a>
+            <h4>Stack Used: ReactJS | NodeJS | SQL</h4>
+            <p style={AboutP}>
+              This app was developed as part of my capstone with Lambda School.  We were given a wireframe of an app that was to be developed and we were given the responsibility of choosing which stack to build the application on as well as the design.  We built the back-end using Node.js and SQL while we built our front-end using React along with a number of other libraries.
+               Styling was completed using styling-components. 
+            </p>
+
+            <a href='https://reactnoteproject.netlify.com/'>
+              <h3 style={headerThree}>Lambda Notes</h3>
+            </a>
+            <h4>Stack Used: ReactJS | NodeJS | SQL</h4>
+            <p style={AboutP}>
+              This application was developed as part of a project week at Lambda School. We were given a mock-up and one week in order to complete this project.  The front-end was built using ReactJS while the back-end was built using NodeJS and SQL.
+              
+            </p>
+            </div>
+
+          <p></p>
+          <p></p>
           {close}
         </article>
 
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">About</h2>
+          <h2 style={Title} className="major">About Me</h2>
           <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <p style={About}>Hello!</p>
+          <p style={AboutP}>My name is Joseph Stanfield and I'm a full stack Web-Developer with experience working with numerous web technologies. What's most important to me is my family, then my career.  I approach my career the same as I do my family, with passion, drive and love. I am always striving to learn new technologies and to become a better web developer. In addition, I have a strong background in team and project management.
+          
+          </p><p style={AboutP}>I began my Full-Stack Web Development 
+          journey in early 2018 when I began at Lambda School. Nine exhausting months later, I am now a qualified Full-Stack Web Developer with specialties in React, Node and SQL. I have completed at least fifteen projects over the past year, starting with the simple and continuing with more 
+          and more complex applications. Take a look at some of them here on my site!  </p>
           {close}
         </article>
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
-            </ul>
-          </form>
-          <ul className="icons">
-            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+          <h2 style={Title} className="major">Contact</h2>
+          <ul style={UL} className="icons">
+          <li><a href="https://www.facebook.com/joe.stanfield.14" className="icon fa-instagram"><span className="label">Facebook</span></a></li>
+            <li><a href="https://www.instagram.com/stickmonster89/" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+            <li><a href="https://www.linkedin.com/in/joseph-stanfield-4a83a757/" className="icon fa-linkedin"><span className="label">Instagram</span></a></li>
+            <li><a href="https://github.com/CookieMonsta89" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li><a href="mailto:jlstan06@yahoo.com?Subject=Portfolio%20Reachout" className="icon fa-envelope"><span className="label">GitHub</span></a></li>
           </ul>
           {close}
         </article>
 
+        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeoutOne' : ''}`} style={{display:'none'}}>
+          <h2 style={Title} className="major">Resume</h2>
+          <div style={Resume}>
+            <iframe src="https://resume.creddle.io/embed/b4hdkq76mtw"
+              width="100%" height="80%" seamless></iframe>
+          </div>
+          {close}
+          
+        </article>
+
       </div>
+
+      
     )
   }
 }
