c9435756f73034f053b438fcb89290dc8bb4580b
CookieMonsta89 <jlstan06@hotmail.com>
2019-01-31 04:08:57 -0500

    changing meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Web Developer Portfolio",
+    author: "Joseph Stanfield",
+    description: "A Web Developers Portfolio"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
